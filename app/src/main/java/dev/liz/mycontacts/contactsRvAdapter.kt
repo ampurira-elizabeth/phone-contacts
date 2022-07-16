6458c546f43839ab245a976087ab3f8f58f6205f
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-07-16 09:07:56 +0300

    images

--- app/src/main/java/dev/liz/mycontacts/contactsRvAdapter.kt
+++ app/src/main/java/dev/liz/mycontacts/contactsRvAdapter.kt
@@ -7,29 +7,34 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.NetworkPolicy
+import com.squareup.picasso.Picasso
+import dev.liz.mycontacts.databinding.ContactListItemBinding
 
 class contactsRvAdapter(var contactList:List<Contacts>):RecyclerView.Adapter<contactViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): contactViewHolder {
-      var itemView=LayoutInflater.from(parent.context).inflate(R.layout.contact_list_item,parent,false)
-        return contactViewHolder(itemView)
+        var binding=ContactListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        var contactViewHolder=contactViewHolder(binding)
+        return contactViewHolder
     }
 
     override fun onBindViewHolder(holder: contactViewHolder, position: Int) {
         var currentcontact=contactList.get(position)
-        holder.tvName.text=currentcontact.name
-        holder.tvEmail.text=currentcontact.email
-        holder.tvLocate.text=currentcontact.address
-        holder.tvPhone.text=currentcontact.phoneNumber
+        holder.binding.tvName.text=currentcontact.name
+        holder.binding.tvEmail.text=currentcontact.email
+        holder.binding.tvLocate.text=currentcontact.address
+        holder.binding.tvPhone.text=currentcontact.phoneNumber
+        Picasso.get().load(currentcontact.image).error(R.drawable.ic_baseline_error_outline_24)
+            .placeholder(R.drawable.ic_baseline_person_24)
+            .networkPolicy(NetworkPolicy.OFFLINE)
+            .resize(300,300).centerCrop()
+            .into(holder.binding.igContact)
     }
 
     override fun getItemCount(): Int {
        return contactList.size
     }
 }
-class contactViewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
-    var tvName=itemView.findViewById<TextView>(R.id.tvName)
-    var tvPhone=itemView.findViewById<TextView>(R.id.tvPhone)
-    var tvEmail=itemView.findViewById<TextView>(R.id.tvEmail)
-    var tvLocate=itemView.findViewById<TextView>(R.id.tvLocate)
-    var igContact=itemView.findViewById<ImageView>(R.id.igContact)
-}
+class contactViewHolder(var binding:ContactListItemBinding):RecyclerView.ViewHolder(binding.root)
+
+
