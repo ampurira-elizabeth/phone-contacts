ee3d2f823c67f79d8622ea9c5306063821f07aff
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-07-16 09:16:20 +0300

    piccaso images

---
+++ app/src/main/java/dev/liz/mycontacts/ViewContactActivity.kt
@@ -0,0 +1,11 @@
+package dev.liz.mycontacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_contact)
+    }
+}
