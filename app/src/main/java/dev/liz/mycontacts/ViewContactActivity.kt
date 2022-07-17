841fcb4d404948fdf5e32b19530e737c7ba7a633
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-07-17 21:09:52 +0300

    image

--- app/src/main/java/dev/liz/mycontacts/ViewContactActivity.kt
+++ app/src/main/java/dev/liz/mycontacts/ViewContactActivity.kt
@@ -2,21 +2,34 @@ package dev.liz.mycontacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Toast
+import com.squareup.picasso.Picasso
+import dev.liz.mycontacts.databinding.ActivityMainBinding
+import dev.liz.mycontacts.databinding.ActivityViewContactBinding
 
 class ViewContactActivity : AppCompatActivity() {
+   lateinit var binding: ActivityViewContactBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityViewContactBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_contact)
+        setContentView(binding.root)
         getExtras()
     }
     fun getExtras(){
         val extras=intent.extras
         val name=extras?.getString("NAME","")
-        val email=extras?.getString("EMAIL","")
-        val address=extras?.getString("ADDRESS","")
+//        val email=extras?.getString("EMAIL","")
+//        val address=extras?.getString("ADDRESS","")
         val phone=extras?.getString("NUMBER","")
-        val image=extras?.getString("IMAGE","")
-        Toast.makeText(this,"$name : $email : $address : $phone : $image",Toast.LENGTH_LONG).show()
+        var image3= binding.ivperson
+
+        Toast.makeText(this, name,Toast.LENGTH_LONG).show()
+        Toast.makeText(this, phone,Toast.LENGTH_LONG).show()
+        binding.tvName2.text=name
+        binding.tvPhone2.text=phone
+        Picasso.get().load(intent.getStringExtra("IMAGE")).into(image3)
+
+//        Toast.makeText(this,"$name : $email : $address : $phone",Toast.LENGTH_LONG).show()
     }
 }
